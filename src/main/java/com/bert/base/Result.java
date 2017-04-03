ed0e20610435b9a3550924f706f73a79dd09ae1d
LiuYanliang <704199225@qq.com>
2017-04-03 23:52:47 +0800

    update1

--- src/main/java/com/bert/base/Result.java
+++ src/main/java/com/bert/base/Result.java
@@ -1,14 +1,11 @@
 /**
  * 文件名:Result.java 
- * 工程名：asfp-server
- * 包名:com.lzz.asfp.api
  * 作者:Administrator
  * 创建时间:2017年3月23日
- * Copyright (C) 2017 绿蜘蛛科技有限公司
  */
 package com.bert.base;
 /**
- * 接口返回结果
+ * 接口返回结果1
  * @author Administrator	
  */
 public class Result {
